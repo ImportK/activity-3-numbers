2e401a7dff11b6a1db63c6fe2ca118f3ceaaed0f
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-08 23:11:56 +0800

    Completed Numbers Operation

---
+++ numbers.js
@@ -0,0 +1,19 @@
+// Storing 
+let firstFavNum = 26;
+let secondFavNum = 14;
+
+// Performing operations.
+let sum = firstFavNum + secondFavNum;
+let difference = firstFavNum - secondFavNum;
+let product = firstFavNum * secondFavNum;
+let quotient = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum;
+
+// Add log results to the console.
+console.log("Sum:" , sum);
+console.log("Difference:" , difference);
+console.log("Product:" , product);
+console.log("Quotient:" , quotient);
+console.log("Modulus:" , modulus);
+console.log("Exponentiation:" , exponentiation);
